7a8983635150cf510f237bc4bb700bed3eea5bb5
Trung <thanhtrung05101996@gmail.com>
2022-04-01 15:52:56 +0700

    add scss and component Navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Nav from './views/Nav'
 const App = () => {
+  let name = 'Yuric'
+
+  let obj = { name: 'Yuric', chanel: 'Hoi Dan IT' }
+  let link = 'https://github.com/ThanhTrung05/react-hook-basic-with-yuric/tree/dev'
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world with React and Yuric</h1>
+        <h1>Hello world with React and {name} in {obj.chanel}</h1>
+        {/* <a href={link}>Visit my channel</a> */}
+        <p>{JSON.stringify(obj)}</p>
       </header>
     </div>
   );
